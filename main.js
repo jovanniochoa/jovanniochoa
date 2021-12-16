2543be66bc052e9715546311167c4bc0acc702c6
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2021-12-16 08:09:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-let animation = anime({
-  targets: '.letter',
-  opacity: 1,
-  translateY: 50, 
-  rotate: {
-    value: 360,
-    duration: 2000,
-    easing: 'easeInExpo'
-  }, 
-  scale: anime.stagger([0.7, 1], {from: 'center'}), 
-  delay: anime.stagger(100, {start: 1000}), 
-  translateX: [-10, 30]
-}); 
+var playPause = anime({
+  targets: 'div.box',
+  translateY: [
+    { value: 200, duration: 500 },
+    { value: 0, duration: 800 }
+  ],
+  rotate:{
+    value: '1turn',
+    easing: 'easeInOutSine'
+  },
+  delay: function(el, i, l){ return i * 1000},
+  autoplay:false,
+  loop:true
+});
+
+document.querySelector('#boxes .play').onclick = playPause.play;
+document.querySelector('#boxes .pause').onclick = playPause.pause;
