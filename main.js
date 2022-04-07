aa1bd81462cf3b79e751f9da38d00f107e11e914
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2022-04-07 16:40:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Animation for Circle Words~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 const myTags = [
-  'JavaScript', 'Java', 'HTML',
-  'C', 'C++', 'WordPress',
-  'Python', 'CSS', 'Prolog',
-  'laTex', 'anime.js', 'MIPS',
-  'JSON', 'MySQL', 'REST API', 'VHDL',
-];
-
-var tagCloud = TagCloud('.content', myTags,{
-
-// radius in px
-radius: 250,
-
-// animation speed
-// slow, normal, fast
-maxSpeed: 'fast',
-initSpeed: 'fast',
-
-// 0 = top
-// 90 = left
-// 135 = right-bottom
-direction: 135,
-
-// interact with cursor move on mouse out
-keep: true
-
-});
-
-//To change the color of text randomly
-var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
-var random_color = colors[Math.floor(Math.random() * colors.length)];
-document.querySelector('.content').style.color = random_color;
+    'JavaScript', 'Java', 'HTML',
+    'C', 'C++', 'WordPress',
+    'Python', 'CSS', 'Prolog',
+    'laTex', 'anime.js', 'MIPS',
+    'JSON', 'MySQL', 'REST API', 'VHDL',
+  ];
+  
+  var tagCloud = TagCloud('.content', myTags,{
+  
+  // radius in px
+  radius: 250,
+  
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'fast',
+  initSpeed: 'fast',
+  
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+  
+  // interact with cursor move on mouse out
+  keep: true
+  
+  });
+  
+  //To change the color of text randomly
+  var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  document.querySelector('.content').style.color = random_color;
+  
